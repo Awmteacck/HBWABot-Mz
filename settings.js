eb43c8f078f88faa83b50b2c81b46e5ba8a4cebe
Awmteacck <147931086+Awmteacck@users.noreply.github.com>
2024-06-26 22:26:35 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,10 +7,10 @@ global.AntiCallBot = true // false a i dah chuan, rawn call-tu che automatic a b
 global.GroupWcMsg = false // false a i dah chuan welcome message i off ang
 global.autoTyping = false // true a i dah chuan i type la a lang ang
 global.autoRecord = false // true a i dah chuan Voice record angin i lang ang
-global.available = true // false a i dah chuan offline angin a awm ang
+global.available = false // false a i dah chuan offline angin a awm ang
 global.public = true // false ah i dah chuan nang chauhin i hmang thei ang
-global.autoread = true // true a i dah chuan message zawng zawng read vek zelin a awm ang 
-global.autoread_status = true // true a i dah chuan auto status view in a awm ang
+global.autoread = false // true a i dah chuan message zawng zawng read vek zelin a awm ang 
+global.autoread_status = false // true a i dah chuan auto status view in a awm ang
 global.default_language = true // default language i hman dawn loh chuan false ah hia  dah tur
 global.mtl_language = true // multi language i hman dawn chuan true ah dah tur
 global.bot_language = 'lus' // I language code dah tur code hman i hriat loh chuan https://herbert70.blogspot.com/p/language-code-for-hbwabot.html?m=1 he link ah hian en thei a ni ang
@@ -31,7 +31,7 @@ global.sessionName = 'session'
 
 // Sticker Pack Name
 global.packname = "Sticker By"
-global.author = "MrBot"
+global.author = "Mr Bot"
 global.hbmods = "https://herbert70.blogspot.com/"
 //_______________________________________________
 global.defaultpp = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60'
