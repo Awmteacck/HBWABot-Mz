ef281ef6b5bc29dbef65257fb7346a761da5c065
Awmtea Polythene <omomacck@yahoo.com>
2024-11-11 22:09:34 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,12 +3,12 @@ const chalk = require("chalk")
 const fs = require("fs")
 
 //aumto presence update
-global.AntiCallBot = false // false a i dah chuan, rawn call-tu che automatic a block a ni lovang
+global.AntiCallBot = true // false a i dah chuan, rawn call-tu che automatic a block a ni lovang
 global.GroupWcMsg = false // false a i dah chuan welcome message i off ang
 global.autoTyping = false // true a i dah chuan i type la a lang ang
 global.autoRecord = false // true a i dah chuan Voice record angin i lang ang
 global.public = true // false ah i dah chuan nang chauhin i hmang thei ang
-global.autoread = false // true a i dah chuan message zawng zawng read vek zelin a awm ang 
+global.autoread = true // true a i dah chuan message zawng zawng read vek zelin a awm ang 
 global.autoread_status = false // true a i dah chuan auto status view in a awm ang
 
 // SET UP LANGUAGE >>>>>>
@@ -19,33 +19,33 @@ global.bot_language = 'lus' // support language [English, Mizo, Indonesia, Russi
 
 
 //info
-global.owner = ['916009920750'] // I phone number in thlak rawh
-global.ownernumber = '916009920750' // I phone number in thlak rawh
-global.ownername = "᭄hazel" // I hming dah tel bawk la
+global.owner = ['918787651195'] // I phone number in thlak rawh
+global.ownernumber = '918787651195' // I phone number in thlak rawh
+global.ownername = "Awmtea Polythene" // I hming dah tel bawk la
 global.location = "India, Mizoram, Aizawl" // I awmna hmun daha
-global.botname = "HBWABot Mizo" // I bot hming tur dah la
-global.ownerweb = "https://youtube.com/@HBMods_Channel"
+global.botname = "Venom" // I bot hming tur dah la
+global.ownerweb = "https://youtube.com/@awmtea-polythene"
 global.themeemoji = '🤖'
-global.creator = "916009920750@s.whatsapp.net" // @ symbol hmaa number hi i phone number in thlak rawh
+global.creator = "918787651195@s.whatsapp.net" // @ symbol hmaa number hi i phone number in thlak rawh
 
-global.ownerNumber = ["916009920750@s.whatsapp.net"]// @ symbol hmaa number hi i phone number in thlak rawh
+global.ownerNumber = ["918787651195@s.whatsapp.net"]// @ symbol hmaa number hi i phone number in thlak rawh
 global.prefixc = [''] // Prefix a tana hman theih loh tur symbolise a awm nuala, i bot a work a lo a nih chuan i thlak mai dawn nia
 global.sessionName = 'session'
 
 // Sticker Pack Name
 global.packname = "Sticker By"
-global.author = "᭄hazel"
+global.author = "Venom"
 
 //BUTTON INPUT 
 global.buttonyt = "SUBSCRIBE"
 global.buttonyturl = "https://youtube.com/@awmtea-polythene"
 global.buttonig = "Follow Us:)"
-global.buttonigurl = "https://www.instagram.com/ign_hazel_"
+global.buttonigurl = "https://www.instagram.com/awmtea._polythene"
 //_______________________________________________
 global.defaultpp = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60'
 //messages
 global.mess = {
-    success: '\n*©Hazel*!', 
+    success: '\n*©Venom*!', 
     admin: 'He features hi chu admin-te chauh hman thei tur a siam a ni!',
     botAdmin: 'Admin-ah min dah hmasa phawt la chuan ka ti thei ang!',
     owner: 'He features hi a neitu chauh hman thei tura siam a ni!',
